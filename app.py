3f4de835b883af899d2a8c0410dfa8dfad0667a4
GBO-Solutions <162637591+GBO-Solutions@users.noreply.github.com>
2024-03-08 15:49:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ with c2:
   st.title(':red[The immediate enemy of the good is the better and its existential enemy is the optimum...]')
 st.header('',divider='red')
 st.title('')
+
+col1,col2 = st.columns([2,1])
+with col1:
+  st.video('./video-0.mp4')
