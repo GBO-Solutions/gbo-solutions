10f945c71ad732e5a30abaa52193c1f8cef04324
GBO-Solutions <162637591+GBO-Solutions@users.noreply.github.com>
2024-03-08 15:57:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,13 @@ st.title('')
 col1,col2 = st.columns([2,1])
 with col1:
   st.video('./video-0.mp4')
+with col2:
+  c1,c2,c3 = st.columns([1,1,1])
+  with c2:
+    st.subheader(':red[SIMPLEX]')
+  c1,c2 = st.columns([1,1])
+  with c1:
+    st.button(':film_projector: :red[watch]',key='simplex1',on_click=video_select,use_container_width=True)
+  with c2:
+    st.button(':lock: :red[download]',key='simplex2',disabled=True,use_container_width=True)
+  st.divider()
