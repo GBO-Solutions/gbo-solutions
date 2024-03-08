fb102e0bc59ab73eafb651c69986ccad5f7e0467
GBO-Solutions <162637591+GBO-Solutions@users.noreply.github.com>
2024-03-08 15:40:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write('Hello World!')
+st.set_page_config(layout='wide')
+c1,c2 = st.columns([1,5])
+with c1:
+  st.image('./logo.png')
+with c2:
+  st.title(':red[The immediate enemy of the good is the better and its existential enemy is the optimum...]')
+st.header('',divider='red')
+st.title('')
