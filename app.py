64156ec15dc3ce127cc4e19518b5aa7610293adf
GBO-Solutions <162637591+GBO-Solutions@users.noreply.github.com>
2024-03-08 16:16:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ with col2:
     st.subheader(':red[SIMPLEX]')
   c1,c2 = st.columns([1,1])
   with c1:
-    st.button(':film_projector: :red[watch]',key='simplex1',use_container_width=True)
+    st.button(':film_projector: :red[watch]',key='Wsimplex',use_container_width=True)
   with c2:
-    st.button(':lock: :red[download]',key='simplex2',disabled=True,use_container_width=True)
+    st.button(':lock: :red[download]',key='Bsimplex',disabled=True,use_container_width=True)
   st.divider()
   # B-F-G-S
   c1,c2,c3 = st.columns([1,1,1])
@@ -30,7 +30,27 @@ with col2:
     st.subheader(':red[B-F-G-S]')
   c1,c2 = st.columns([1,1])
   with c1:
-    st.button(':film_projector: :red[watch]',key='bfgs1',use_container_width=True)
+    st.button(':film_projector: :red[watch]',key='Wbfgs',use_container_width=True)
   with c2:
-    st.button(':lock: :red[download]',key='bfgs2',disabled=True,use_container_width=True)
+    st.button(':lock: :red[download]',key='Bbfgs',disabled=True,use_container_width=True)
+  st.divider()
+    # GENETIC
+  c1,c2,c3 = st.columns([1,1,1])
+  with c2:
+    st.subheader(':red[GENETIC]')
+  c1,c2 = st.columns([1,1])
+  with c1:
+    st.button(':film_projector: :red[watch]',key='Wgenetic',use_container_width=True)
+  with c2:
+    st.button(':lock: :red[download]',key='Bgenetic',disabled=True,use_container_width=True)
+  st.divider()
+    # ADJOINT
+  c1,c2,c3 = st.columns([1,1,1])
+  with c2:
+    st.subheader(':red[ADJOINT]')
+  c1,c2 = st.columns([1,1])
+  with c1:
+    st.button(':film_projector: :red[watch]',key='Wadjoint',use_container_width=True)
+  with c2:
+    st.button(':lock: :red[download]',key='Badjoint',disabled=True,use_container_width=True)
   st.divider()
