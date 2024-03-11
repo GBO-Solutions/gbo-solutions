4eb727f4ebee0f7a5a93c514eedb9bd14beef76c
GBO-Solutions <162637591+GBO-Solutions@users.noreply.github.com>
2024-03-11 16:53:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 
 st.set_page_config(layout='wide')
-c1,c2 = st.columns([1.25,5])
+c1,c2 = st.columns([1,5])
 with c1:
   st.image('./logo.png')
 with c2:
-  st.title(':red[The immediate enemy of the Good is the Better]')
-  st.title(':red[and its existential enemy is the Optimum...]')
+  st.title(':red[The immediate enemy of the Good is the Better and its existential enemy is the Optimum...]')
 st.header('',divider='red')
 st.title('')
 
