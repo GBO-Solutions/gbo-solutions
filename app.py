5aa201d5b07aab53cc7f2b1a99cd5957b89bb702
GBO-Solutions <162637591+GBO-Solutions@users.noreply.github.com>
2024-03-29 01:07:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ with c1:
   st.image('./logo.png')
 with c2:
   st.title(':red[The prime rival of Good is Better, and its arch rival is Optimum...]')
-st.header('',divider='red')
+  st.header('',divider='red')
 st.title('')
 
 col1,col2 = st.columns([2,1])
