98727e0a3ac4a7c1e7b0586468b037eeddc57693
GBO-Solutions <162637591+GBO-Solutions@users.noreply.github.com>
2024-05-17 18:11:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 """
 page_bg_img = 
 <style>
-[data-testid = "stAppViewContainer"] {background-color: #e5e5f7;}
+   [data-testid = "stAppViewContainer"] {background-color: #e5e5f7;}
 </style>
 """
 
